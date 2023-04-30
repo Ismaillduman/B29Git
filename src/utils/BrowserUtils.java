820d4ca08aa54e93c13ff3ac1d8e2a8b5e857a1c
Ismailduman <issmailduman@gmail.com>
2023-04-30 17:29:48 +0200

    hi git

--- src/utils/BrowserUtils.java
+++ src/utils/BrowserUtils.java
@@ -1,7 +1,12 @@
 package utils;
 
 public class BrowserUtils {
+
+    public static void main(String[] args) {
+        System.out.println("Hi git");
+    }
     //open browser util
 
     // some news..
 }
+
